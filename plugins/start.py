1de028eb975958804c8c98dee8aaa923380a11a3
Charlie-jinn-DQ <65146680+Charlie-jinn-DQ@users.noreply.github.com>
2020-07-07 22:10:59 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,7 +9,7 @@ from config import Config
 from telegram import ParseMode
 
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text=f"""Hi [{update.message.from_user.first_name}](tg://user?id={update.message.from_user.id}) I am Attach Bot. 
+  context.bot.send_message(chat_id=update.effective_chat.id, text=f"""Hi I am Attach Bot. 
                            
                            'I can attach medias to your long text.'
                            
