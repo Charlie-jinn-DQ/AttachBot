a6683d22a09395fe86681dd32fe1aa007d80c49d
Charlie-jinn-DQ <65146680+Charlie-jinn-DQ@users.noreply.github.com>
2020-07-09 12:51:26 +0530

    Update tg_attach.py

--- plugins/tg_attach.py
+++ plugins/tg_attach.py
@@ -15,8 +15,8 @@ def attach(update, context):
 For more help watch this video,
 👉 t.me/DX_Botz/170
     
-join our Support Group,
-👉 @Dx_Support""")
+Join our Support Group,
+👉 @DX_Support""")
   else:
     m = context.bot.forward_message("@" + Config.CHANNEL_USERNAME, update.effective_chat.id, update.message.reply_to_message.message_id)
     m_id = m.message_id
